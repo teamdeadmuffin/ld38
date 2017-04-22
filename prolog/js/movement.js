9705fdf52da129d021d81ed3e8cc55050090d599
Anne Ogborn <annie@thelginworks.com>
2017-04-21 22:28:13 -0700

    first physics

--- prolog/js/movement.js
+++ prolog/js/movement.js
@@ -1,14 +1,46 @@
 var gamelooptimer;
+var catvx = 12.0;
+var catvy = 6.0;
+var gconst = 800000.0;
+var flying = true;
 
 function gameloop() {
-	$("#cat").attr("cx", function(n, v) {
-			return String(Number(v) + 3.0);
+	var cx = Number($("#cat").attr("cx"));
+	var cy = Number($("#cat").attr("cy"));
+
+
+// compute the local g
+
+        var gx = 0.0;
+        var gy = 0.0;
+
+	$("#asteroids circle").each(function() {
+			var acx = Number($(this).attr("cx"));
+			var acy = Number($(this).attr("cy"));
+			var grnd = Number($(this).attr("r"));
+
+			var r = Math.sqrt((cx - acx)*(cx - acx) + (cy - acy) * (cy - acy) );
+			var g = gconst / r / r;
+			gx = gx + g * (acx - cx) / r;
+			gy = gy + g * (acy - cy) / r;
+			if(r < grnd) {
+				flying = false;
+			}
 		});
+
+	catvx = catvx + gx;
+	catvy = catvy + gy;
+	if(flying) {
+		$("#cat").attr("cx", String(cx + catvx));
+		$("#cat").attr("cy", String(cy + catvy));
+	} else {
+		catvx = 0.0;
+		catvy = 0.0;
+	}
+
+
 };
 
 $(function() {
-
-     alert('called');
-
-     window.setInterval(gameloop, 250);
+     window.setInterval(gameloop, 100);
 });
