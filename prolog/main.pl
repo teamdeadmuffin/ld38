2d57b7fe741d6576e52e20aa372f25871c9109aa
Anne Ogborn <annie@thelginworks.com>
2017-04-24 01:01:58 -0700

    earth placed

--- prolog/main.pl
+++ prolog/main.pl
@@ -4,7 +4,6 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/html_write)).
-:- use_module(library(http/html_quasiquotations)).
 :- use_module(library(http/html_head)).
 :- use_module(library(http/http_files)).
 
@@ -56,15 +55,33 @@ control_panel -->
     },
     html([div(input([id(keysink), type(text)], [])),
          audio([loop(true), src(Src), autoplay(true)], []),
-         audio([loop(false), src('/audio/crystal0.mp3'), autoplay(false)], [])
+         audio([src('/audio/crystal0.mp3')], [])
          ]).
 
 main_svg -->
+    {
+         locate_earth(EarthX, EarthY),
+    format(string(JS), '<script>var earthx=~w;  var earthy=~w;</script>', [EarthX, EarthY])
+    },
+    html(\[JS]),
     contents_of_file(icons('svgheader.svf')),
     contents_of_file(icons('symbols.svf')),
     contents_of_file(icons('scrollmehdr.svf')),
+    {
+        format(string(S), '<use xlink:href="#earth_1_"  id="earth" width="900.8" height="1024" x="-450.4" y="-512" transform="translate(~w ~w)" style="overflow:visible;"/><g id="asteroids" >', [EarthX, EarthY])
+    },
+    html(\[S]),
     asteroids,
     contents_of_file(icons('svgtail.svf')).
+
+locate_earth(EarthX, EarthY) :-
+    repeat,
+    random_between(4096, 57344, EarthX),
+    random_between(4096, 57344, EarthY),
+    (   EarthX < 24567 ; EarthX > 40960),
+    (   EarthY < 24567 ; EarthY > 40960).
+
+
 
 asteroids -->
     asteroids(300, []).
@@ -84,18 +101,19 @@ asteroids(N, AsteroidsSoFar) -->
       random_between(A, B, X),
       random_between(A, B, Y),
       debug(game, 'trying ~w ~w for ~w', [X, Y, N]),
-      maplist(safe_location(X, Y), [32786.0-32786.0 | AsteroidsSoFar]), % include the cat
-      random_between(1, 6, M),
-      random_between(0, 359, R),
       RadScale is (6.0 + 6.0 * random_float) / 10.0,
       GRad is 200.0 * RadScale + 60.0,
+      Exclusion is 200.0 * RadScale * 2.0,
+      maplist(safe_location(X, Y, Exclusion), [32786.0-32786.0-360.0 | AsteroidsSoFar]), % include the cat
+      random_between(1, 6, M),
+      random_between(0, 359, R),
       format(string(S) ,
  '<use xlink:href="#asteroid~w"  class="asteroid" id="a~w" gameid="~w" width="404" height="404" x="-202" y="-201.999" transform="translate(~w ~w) scale(~w) rotate(~w 0 0)" gamerad="~w" gamex="~w" gamey="~w" overflow="visible"/>~n', [M, N, N, X, Y, RadScale, R, GRad, X, Y])
     },
     html(\[S]),
     crystals_for(N, X, Y, RadScale, _Points, _Oxygen),   % DARN IT JAN
     !,
-    asteroids(NN, [X-Y | AsteroidsSoFar]).
+    asteroids(NN, [X-Y-Exclusion | AsteroidsSoFar]).
 
 crystals_for(Asteroid, X, Y, RadScale, Points, Oxygen) -->
     {
@@ -147,9 +165,9 @@ powerup_(oxygen, Asteroid, X, Y, R, S) :-
       format(string(S) ,
              '<use xlink:href="#oxygen" class="powerup oxygen"  width="56.667" height="144.778" x="-28.334" y="-72.389" transform="translate(~w ~w) rotate(~w 0 0) translate(0 ~w) rotate(~w 0 0)" asteroid="~w" overflow="visible"/>', [X, Y,Rot, RR, Rot2, Asteroid]).
 
-safe_location(X, Y, AX-AY) :-
+safe_location(X, Y, MyExclusion, AX-AY-R) :-
     RSQ is (X - AX)* (X - AX) + (Y - AY) * (Y - AY),
-    RSQ > 640000.  % 800 pixels distance
+    RSQ > (R + MyExclusion) * (R + MyExclusion).  % 800 pixels distance
 
 contents_of_file(File) -->
     {
